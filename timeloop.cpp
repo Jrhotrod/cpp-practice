0771894d379580272610aa6c12c0de9c9d51e9ea
Jrhotrod <juniorhotrod@gmail.com>
2020-07-06 19:09:41 -0600

    initial code for the Time Loop exercise

---
+++ timeloop.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Jrhotrod on 7/6/2020.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int i;
+    cout << "Input: ";
+    cin >> i;
+
+    int x = 0;
+    while (x <= i; x++){
+        cout << "\n" << x << " Abracadabra";
+    }
+}
